3ece92ef694e271ba8eab3bb8d89f2f2bb4b9a29
Muhammad Musa Mueed <musavuswh@gmail.com>
2024-11-21 12:34:31 +0500

    first commit

---
+++ Fetch API/Part 1.js
@@ -0,0 +1,27 @@
+/*
+
+Fetch API - provides an interface for fetching (sending/receiving) resources.
+
+It uses Request & Response objects.
+
+The 'fetch()' method is used to fetch a resource (data).
+
+
+e.g;
+
+let promise = fetch(url, [options]);
+
+
+
+promises plays a imp role in Fetching API(s).
+
+                     ________
+request  ------>    |   API  |
+response <------    ----------
+
+Note:
+API returns us response. API actually works on the Request & Response Cycle (API Calls are done in JavaScript, using "Fetch API").
+
+*/
+
+
